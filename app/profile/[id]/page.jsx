6d304fd048d4ff39d3e21358c8068272ddb6a379
Anne Lee <annelee@annes-macbook-air.home>
2024-02-16 14:08:54 +0100

    feat: access to user profile page

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,31 @@
+'use client'
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'next/navigation'
+import Profile from '@components/Profile'
+
+const page = ({ params }) => {
+  const searchParams = useSearchParams()
+  const userName = searchParams.get('name')
+
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${params?.id}/posts`)
+      const data = await response.json()
+      setPosts(data)
+    }
+    if (params?.id) fetchPosts()
+  }, [params.id])
+
+
+  return (
+    <Profile
+      name={`${userName}'s`}
+      desc={`Welcome to ${userName}'s personalized profile page`}
+      data={posts}
+    />
+  )
+}
+
+export default page
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import Image from 'next/image'
 import { useSession } from 'next-auth/react'
 import { usePathname, useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
@@ -19,10 +20,18 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     setTimeout(() => setCopied(''), 3000)
   }
 
+  const handleProfileClick = () => {
+    // if (post.creator._id === session?.user.id) return router.push('/profile')
+    return router.push(`/profile/${post.creator._id}?name=${post.creator.username}`)
+  }
+
   return (
     <div className='prompt_card'>
       <div className='flex justify-between items-start gap-5'>
-        <div className='flex-1 flex justify-start items-center gap-3 cursor-pointer'>
+        <div
+          className='flex-1 flex justify-start items-center gap-3 cursor-pointer'
+          onClick={handleProfileClick}
+        >
           <Image
             src={post.creator.image}
             alt='user_image'
